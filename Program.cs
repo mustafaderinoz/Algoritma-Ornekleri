750df89c87c94fe73b27d204f439f877045e2c42
mustafaderinoz <mustafa.derinoz99@gmail.com>
2025-10-22 15:38:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubAlgoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+            int aranan = 12;
+            int left = 0;
+            int right = dizi.Length - 1;
+
+            int sonuc = RecursiveBinaryArama(dizi, aranan, left, right);
+            if (sonuc != -1) Console.WriteLine("Aranan {0} sayısi {1}. indekste bulundu", aranan, sonuc);
+            else Console.WriteLine("Aranan sayı bulunamadı");
+
+
+
+        }
+        static int RecursiveBinaryArama(int[] dizi, int aranan, int left, int right)
+        {
+            if (left <= right)
+            {
+                int orta = left + (right - left) / 2;
+
+                if (dizi[orta] == aranan) return orta;
+
+                if (dizi[orta] > aranan) return RecursiveBinaryArama(dizi, aranan, left, orta - 1);
+                else return RecursiveBinaryArama(dizi, aranan, left + 1, right);
+            }
+            return -1;
+        }
+    }
+}
