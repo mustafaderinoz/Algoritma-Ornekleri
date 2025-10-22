7fa935ae92fa755aec8c57d07d48eca0afd021b4
mustafaderinoz <mustafa.derinoz99@gmail.com>
2025-10-22 15:35:53 +0300

    Update and rename BinarySearchNormalRecursiveCozum.cs to BinarySearch.cs

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -10,8 +10,6 @@ namespace githubAlgoritma
     {
         static void Main(string[] args)
         {
-            // normal çözüm
-            Console.WriteLine("Normal Çözüm");
             int[] dizi = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
             int aranan = 12;
 
@@ -19,17 +17,6 @@ namespace githubAlgoritma
 
             if (sonuc != -1) Console.WriteLine("Aranan {0} sayısi {1}. indekste bulundu", aranan, sonuc);
             else Console.WriteLine("sayı bulunamadı");
-
-            // recursive çözüm
-            Console.WriteLine();
-            Console.WriteLine("Recursive Çözüm");
-
-            int left = 0;
-            int right = dizi.Length - 1;
-
-            int recursivesonuc = RecursiveBinaryArama(dizi, aranan, left, right);
-            if (recursivesonuc != -1) Console.WriteLine("Aranan {0} sayısi {1}. indekste bulundu", aranan, recursivesonuc);
-            else Console.WriteLine("Aranan sayı bulunamadı");
         }
 
         static int BinaryArama(int[] dizi, int aranan)
@@ -55,19 +42,8 @@ namespace githubAlgoritma
             return -1;
         }
 
-        static int RecursiveBinaryArama(int[] dizi, int aranan, int left, int right)
-        {
-            if (left <= right)
-            {
-                int orta = left + (right - left) / 2;
-
-                if (dizi[orta] == aranan) return orta;
-
-                if (dizi[orta] > aranan) return RecursiveBinaryArama(dizi, aranan, left, orta - 1);
-                else return RecursiveBinaryArama(dizi, aranan, left + 1, right);
-            }
-            return -1;
-        }
+     
     }
 }
+
 
